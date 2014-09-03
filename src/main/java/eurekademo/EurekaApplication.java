4d0101e720ee2d4c35c56d839431ff28c577a14a
Dave Syer <dsyer@pivotal.io>
2014-09-03 10:10:21 +0100

    Make Eureka server embeddable

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -1,19 +1,10 @@
 package eurekademo;
 
-import javax.servlet.Filter;
-
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.google.common.collect.Lists;
-import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 /**
  *
@@ -23,39 +14,11 @@ import com.sun.jersey.spi.container.servlet.ServletContainer;
 @ComponentScan
 @Configuration
 @EnableAutoConfiguration
-public class EurekaApplication extends WebMvcConfigurerAdapter {
+@EnableEurekaServer
+public class EurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
-	}
-
-	@Bean
-	public FilterRegistrationBean jersey() {
-		FilterRegistrationBean bean = new FilterRegistrationBean();
-		bean.setFilter(new ServletContainer());
-		bean.setOrder(Ordered.LOWEST_PRECEDENCE);
-		bean.addInitParameter("com.sun.jersey.config.property.packages",
-				"com.netflix.discovery;com.netflix.eureka");
-		bean.setUrlPatterns(Lists.newArrayList("/v2/*"));
-		return bean;
-	}
-
-	// TODO: remove this when we upgrade to Boot 1.1.6
-	@Bean
-	public FilterRegistrationBean metricFilterRegistration(@Qualifier("metricFilter") Filter filter) {
-		FilterRegistrationBean bean = new FilterRegistrationBean();
-		bean.setFilter(filter);
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
-	}
-
-	// TODO: remove this when we upgrade to Boot 1.1.6
-	@Bean
-	public FilterRegistrationBean traceFilterRegistration(@Qualifier("webRequestLoggingFilter") Filter filter) {
-		FilterRegistrationBean bean = new FilterRegistrationBean();
-		bean.setFilter(filter);
-		bean.setOrder(Ordered.LOWEST_PRECEDENCE - 10);
-		return bean;
 	}
 
 }
