7d1a1469debd3d2333ebd985454f19ec0e20222d
Spencer Gibb <spencer@gibb.us>
2014-12-04 14:37:47 -0700

    Use @EnableDiscoveryClient

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -2,7 +2,7 @@ package eurekademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @EnableEurekaServer
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class EurekaApplication {
 
 	public static void main(String[] args) {
