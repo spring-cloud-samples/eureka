fb0433412ede8a53380a39709881bb7d5ac5da57
Spencer Gibb <spencer@gibb.us>
2015-01-22 10:43:12 -0700

    use @SpringBootApplication

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -1,20 +1,16 @@
 package eurekademo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  *
  * @author Gunnar Hillert
  *
  */
-@ComponentScan
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaServer
 @EnableDiscoveryClient
 public class EurekaApplication {
