1de7c89cf3f79e4707dbabe91ea60eb06f2268aa
Spencer Gibb <spencer@gibb.us>
2015-01-22 10:38:00 -0700

    package as a war

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -1,23 +1,20 @@
 package eurekademo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  *
  * @author Gunnar Hillert
  *
  */
-@ComponentScan
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaServer
 @EnableDiscoveryClient
-public class EurekaApplication {
+public class EurekaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
