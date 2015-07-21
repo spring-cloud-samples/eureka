bd21273ad9623ec403f82c9394a4d029e2dd4fba
Dave Syer <dsyer@pivotal.io>
2015-07-21 09:22:01 +0100

    Remove syserr debug printing

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -1,12 +1,9 @@
 package eurekademo;
 
-import java.io.IOException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.core.io.ClassPathResource;
 
 /**
  *
@@ -19,7 +16,6 @@ import org.springframework.core.io.ClassPathResource;
 public class EurekaApplication {
 
 	public static void main(String[] args) throws Exception {
-		System.err.println(new ClassPathResource("static/eureka/css/wro.css").getURL());
 		SpringApplication.run(EurekaApplication.class, args);
 	}
 
