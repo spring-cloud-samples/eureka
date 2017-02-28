ff513fe311ba3ce262027f40238a66ed2a164a6a
Spencer Gibb <spencer@gibb.us>
2017-02-28 11:14:10 -0700

    remove unneeded `@EnableDiscoveryClient`

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -2,7 +2,6 @@ package eurekademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -12,7 +11,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-@EnableDiscoveryClient
 public class EurekaApplication {
 
 	public static void main(String[] args) throws Exception {
