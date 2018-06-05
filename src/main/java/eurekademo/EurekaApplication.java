ea6c594b23b35b4700ca11ad5932648c7754d10b
Ryan Baxter <rbaxter@pivotal.io>
2018-06-05 14:53:36 -0400

    Add security to demonstrate usage

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -3,6 +3,9 @@ package eurekademo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  *
@@ -15,6 +18,16 @@ public class EurekaApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().ignoringAntMatchers("/eureka/**");
+			super.configure(http);
+		}
 	}
 
 }
