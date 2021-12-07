559408bf26c4ebb5425c67c47533cb90e039c238
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-12-07 19:01:16 +0100

    Upgrade to Cloud 2021, Boot 2.6.1 and Gradle 7.3.1. Remove unnecessary config.

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
 
