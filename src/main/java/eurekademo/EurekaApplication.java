672b400177476ce3cb92a638cf568825d20a94a2
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2023-06-16 16:23:05 +0200

    Update and upgrade to `2022.0.x`.

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -3,31 +3,29 @@ package eurekademo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
- *
  * @author Gunnar Hillert
- *
+ * @author Olga Maciaszek-Sharma
  */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 
-	@EnableWebSecurity
-	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().ignoringAntMatchers("/eureka/**");
-			super.configure(http);
-		}
-	}
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        http.csrf().ignoringRequestMatchers("/eureka/**");
+        return http.build();
+    }
 }
